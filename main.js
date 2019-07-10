0ab53d223d76acf3634d3a0d295d64ef1fab7aa8
郷大助 <daisukegoh@goudaisukes-MacBook-Pro.local>
2019-07-10 23:51:54 +0900

    intial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbyes.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+// "Goodbye"
+goodbyes.goodbyeEnglish();
+
+// "Sayonara"
+goodbyes.goodbyeJapanese();
